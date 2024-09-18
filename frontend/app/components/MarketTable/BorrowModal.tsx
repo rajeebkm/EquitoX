31412d51acb7f9113d087feb0a29d8be11aa8176
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-09-18 23:19:53 +0530

    feat:add cross chain functionality in borrow modal

--- frontend/app/components/MarketTable/BorrowModal.tsx
+++ frontend/app/components/MarketTable/BorrowModal.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from 'react';
 import Image from 'next/image';
+import { ChainCard } from '../ChainCard';
 
 interface Token {
     imageUrl: string;
@@ -71,6 +72,7 @@ interface BorrowModalProps {
     availableTokens: Token[];
 }
 
+
 const BorrowModal: React.FC<BorrowModalProps> = ({ isOpen, onClose, availableTokens }) => {
     const [collateralAmount, setCollateralAmount] = useState<string>('');
     const [borrowAmount, setBorrowAmount] = useState<string>('');
@@ -83,6 +85,7 @@ const BorrowModal: React.FC<BorrowModalProps> = ({ isOpen, onClose, availableTok
 
     const handleMaxCollateralClick = () => setCollateralAmount(selectedCollateralToken.walletBalance);
     const handleMaxBorrowClick = () => setBorrowAmount('MAX_BORROW');
+
 
     const handleTokenChange = (token: Token, type: 'collateral' | 'borrow') => {
         if (type === 'collateral') {
@@ -98,8 +101,8 @@ const BorrowModal: React.FC<BorrowModalProps> = ({ isOpen, onClose, availableTok
 
 
     return (
-        <div className="fixed inset-0 z-50 overflow-y-auto flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
-            <div className="relative bg-gray-800 text-white rounded-lg p-4 w-full max-w-lg shadow-2xl">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+            <div className="relative bg-gray-800 text-white rounded-lg p-4 w-full max-w-3xl h-[80vh] shadow-2xl overflow-y-auto">
                 <div className="flex justify-between items-center mb-6">
                     <h2 className="text-2xl font-semibold">Borrow {selectedBorrowToken.token}</h2>
                     <button
@@ -110,19 +113,35 @@ const BorrowModal: React.FC<BorrowModalProps> = ({ isOpen, onClose, availableTok
                     </button>
                 </div>
 
+
                 <div className="flex flex-col mb-4 bg-gray-700 p-4 rounded-lg">
                     <label className="block text-sm font-medium mb-2">Collateral Market</label>
+
+
                     <div className="relative mb-4">
-                        <div
-                            className="bg-gray-900 text-white p-3 rounded-lg w-full cursor-pointer flex items-center justify-between"
-                            onClick={() => setCollateralDropdownOpen(!collateralDropdownOpen)}
-                        >
-                            <TokenDisplay
-                                imageUrl={selectedCollateralToken.imageUrl}
-                                token={selectedCollateralToken.token}
-                            />
-                            <span className="text-gray-400">{collateralDropdownOpen ? '▲' : '▼'}</span>
+                        <div className="flex space-x-3">
+
+                            <div
+                                className="bg-gray-900 text-white p-3 rounded-lg flex-1 cursor-pointer flex items-center justify-between"
+                                onClick={() => setCollateralDropdownOpen(!collateralDropdownOpen)}
+                            >
+                                <TokenDisplay
+                                    imageUrl={selectedCollateralToken.imageUrl}
+                                    token={selectedCollateralToken.token}
+                                />
+                                <span className="text-gray-400">{collateralDropdownOpen ? '▲' : '▼'}</span>
+                            </div>
+
+
+
+                            <div
+                                className="bg-gray-900 text-white p-3 rounded-lg flex-1 flex items-center justify-center"
+                            >
+                                <ChainCard mode="from" />
+                            </div>
                         </div>
+
+
                         {collateralDropdownOpen && (
                             <div className="absolute top-full left-0 w-full bg-gray-900 border border-gray-600 mt-1 rounded-lg z-10">
                                 {availableTokens.map((token) => (
@@ -144,6 +163,8 @@ const BorrowModal: React.FC<BorrowModalProps> = ({ isOpen, onClose, availableTok
                             </div>
                         )}
                     </div>
+
+
                     <InputWithMaxButton
                         label="Collateral Amount"
                         placeholder={`min 10 ${selectedCollateralToken.token}`}
@@ -154,18 +175,26 @@ const BorrowModal: React.FC<BorrowModalProps> = ({ isOpen, onClose, availableTok
                     />
                 </div>
 
+
                 <div className="flex flex-col mb-4 bg-gray-700 p-4 rounded-lg">
                     <label className="block text-sm font-medium mb-2">Borrow Market</label>
                     <div className="relative mb-4">
-                        <div
-                            className="bg-gray-900 text-white p-3 rounded-lg w-full cursor-pointer flex items-center justify-between"
-                            onClick={() => setBorrowDropdownOpen(!borrowDropdownOpen)}
-                        >
-                            <TokenDisplay
-                                imageUrl={selectedBorrowToken.imageUrl}
-                                token={selectedBorrowToken.token}
-                            />
-                            <span className="text-gray-400">{borrowDropdownOpen ? '▲' : '▼'}</span>
+                        <div className="flex space-x-3">
+                            <div
+                                className="bg-gray-900 text-white p-3 rounded-lg w-full cursor-pointer flex items-center justify-between"
+                                onClick={() => setBorrowDropdownOpen(!borrowDropdownOpen)}
+                            >
+                                <TokenDisplay
+                                    imageUrl={selectedBorrowToken.imageUrl}
+                                    token={selectedBorrowToken.token}
+                                />
+                                <span className="text-gray-400">{borrowDropdownOpen ? '▲' : '▼'}</span>
+                            </div>
+                            <div
+                                className="bg-gray-900 text-white p-3 rounded-lg flex-1 flex items-center justify-center"
+                            >
+                                <ChainCard mode="to" />
+                            </div>
                         </div>
                         {borrowDropdownOpen && (
                             <div className="absolute top-full left-0 w-full bg-gray-900 border border-gray-600 mt-1 rounded-lg z-10">
@@ -198,6 +227,7 @@ const BorrowModal: React.FC<BorrowModalProps> = ({ isOpen, onClose, availableTok
                     />
                 </div>
 
+
                 <div className="flex justify-between items-center mb-4 text-sm bg-gray-700 p-4 rounded-lg">
                     <div className="flex flex-col items-center">
                         <span className="text-gray-400">Fees</span>
@@ -208,11 +238,14 @@ const BorrowModal: React.FC<BorrowModalProps> = ({ isOpen, onClose, availableTok
                         <span className="font-semibold">11.50%</span>
                     </div>
                 </div>
+
+
                 <div className="mb-4">
                     <p className="text-sm text-white">
                         You have selected a native token as collateral which will be converted to rtokens 1rSTRK = 1.0188STRK
                     </p>
                 </div>
+
 
                 <button className="w-full bg-blue-600 hover:bg-blue-500 p-2 rounded-lg text-lg font-semibold transition duration-200">
                     Borrow
@@ -224,8 +257,3 @@ const BorrowModal: React.FC<BorrowModalProps> = ({ isOpen, onClose, availableTok
 
 
 export default BorrowModal;
-
-
-
-
-
