4f2e43bbdb058aace8c87232c2c18d25cc8f9ec1
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-09-18 23:12:54 +0530

    fix:wsProvider link

--- frontend/app/lib/equito-client.ts
+++ frontend/app/lib/equito-client.ts
@@ -4,8 +4,8 @@ let client: EquitoClient;
 
 export const getEquitoClient = async () => {
   if (!client) {
-    const wsProvider = process.env.NEXT_TESTNET_WS_ENDPOINT;
-    const archiveWsProvider = process.env.NEXT_TESTNET_ARCHIVE_WS_ENDPOINT;
+    const wsProvider ="wss://aleph.testequito.live";
+    const archiveWsProvider = "wss://aleph.testequito.live";
     if (!wsProvider || !archiveWsProvider) {
       throw new Error(
         "Missing environment variables NEXT_TESTNET_WS_ENDPOINT and NEXT_TESTNET_ARCHIVE_WS_ENDPOINT for Equito client"
